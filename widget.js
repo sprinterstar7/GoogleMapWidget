d5befd4742ba927b7936d3e6611564127db03c5a
Emmett Baker <baker@rdacorp.com>
2016-11-23 16:11:59 -0500

    Restore zoom

--- widget.js
+++ widget.js
@@ -249,19 +249,21 @@ prism.run(['plugin-googleMapsWidget.services.helperService', 'plugin-googleMapsW
 
 								switch(widget.mapSettings.zoomLevel)
 								{
+									case 6:
+									case 7:
 									case 8:
 									case 9:
 									case 10:
 									case 11:
-									case 12: 
+									case 12: column = "1";
+										break;
 									case 13:
-									case 14: column = "1";
-										break;
+									case 14:
 									case 15:
-									case 16: 
-									case 17:
-									case 18: column = "2";
+									case 16: column = "2";
 										break;
+									case 17:
+									case 18:
 									case 19:
 									case 20: column = "3";
 										break;
